f0b7af201e24089c829ecfc39b8f2deee274978a
Yaddalapalli-Charan-Kumar-Naidu <charankumarnaidu2004@gmail.com>
2025-04-12 23:42:53 +0530

    project showcase section implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './components/Hero/Hero'
+import ShowcaseSection from './components/ShowcaseSection'
 
 function App() {
   
   return (
     <>
       <Hero/>
+      <ShowcaseSection/>
     </>
   )
 }
---
+++ src/components/ShowcaseSection.jsx
@@ -0,0 +1,83 @@
+import React, { useRef } from 'react'
+import {gsap} from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+const ShowcaseSection = () => {
+    const sectionRef=useRef(null);
+    const project1Ref=useRef(null);
+    const project2Ref=useRef(null);
+    const project3Ref=useRef(null);
+    const projects=[project1Ref.current,project2Ref.current,project3Ref.current];
+    projects.forEach((card,index)=>{
+        gsap.fromTo(
+            card,
+            {
+                y:50,
+                opacity:0
+            },
+            {
+                y:0,
+                opacity:1,
+                duration:1,
+                delay:0.3 *(index+1),
+                scrollTrigger:{
+                    trigger:card,
+                    start:'top bottom-=100'
+                }
+            }
+        )
+    })
+
+    useGSAP(()=>{
+        gsap.fromTo(
+            sectionRef.current,
+            {opacity:0},
+            {opacity:1,duration:1.5}
+        )
+    },[])
+
+  return (
+    <section ref={sectionRef} id='work' className='app-showcase'>
+        <div className='w-full'>
+            <div className='showcaselayout'>
+                <div className='first-project-wrapper' ref={project1Ref}>
+                    <div className='image-wrapper'>
+                        <img src="/images/project1.png"/>
+                    </div>
+                    <div className='text-content'>
+                        <h2>Devhub a website for developers to connect</h2>
+                        <p className='text-white-50 md:text-xl'>A place for developer to chat and connect with each other</p>
+                    </div>
+                </div>
+                <div className="project-list-wrapper overflow-hidden">
+                    <div className="project" ref={project2Ref}>
+                        <div className='image-wrapper bg-[#ffefdb]'>
+                            <img src="/images/project2.png" alt="project2"/>
+                            
+                        </div>
+                        <h2>
+                                Resume Craft
+                            </h2>
+                    </div>
+                    <div className="project" ref={project3Ref}>
+                        <div className='image-wrapper bg-[#ffe7eb]'>
+                            <img src="/images/project3.png" alt="project2"/>
+                            
+                        </div>
+                        <h2>
+                                Portfolio
+                            </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default ShowcaseSection
