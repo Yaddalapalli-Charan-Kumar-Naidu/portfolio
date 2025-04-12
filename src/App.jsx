ccee9606cb1ae773c62b2d61c9d485cd475b80b0
Yaddalapalli-Charan-Kumar-Naidu <charankumarnaidu2004@gmail.com>
2025-04-12 23:46:49 +0530

    project section changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './components/Hero/Hero'
 import ShowcaseSection from './components/ShowcaseSection'
+import Navbar from './components/Navbar'
 
 function App() {
   
   return (
     <>
+      <Navbar/>
       <Hero/>
       <ShowcaseSection/>
     </>
--- src/components/ShowcaseSection.jsx
+++ src/components/ShowcaseSection.jsx
@@ -12,28 +12,29 @@ const ShowcaseSection = () => {
     const project1Ref=useRef(null);
     const project2Ref=useRef(null);
     const project3Ref=useRef(null);
-    const projects=[project1Ref.current,project2Ref.current,project3Ref.current];
-    projects.forEach((card,index)=>{
-        gsap.fromTo(
-            card,
-            {
-                y:50,
-                opacity:0
-            },
-            {
-                y:0,
-                opacity:1,
-                duration:1,
-                delay:0.3 *(index+1),
-                scrollTrigger:{
-                    trigger:card,
-                    start:'top bottom-=100'
-                }
-            }
-        )
-    })
-
+    
+    
     useGSAP(()=>{
+        const projects=[project1Ref.current,project2Ref.current,project3Ref.current];
+        projects.forEach((card,index)=>{
+            gsap.fromTo(
+                card,
+                {
+                    y:50,
+                    opacity:0
+                },
+                {
+                    y:0,
+                    opacity:1,
+                    duration:1,
+                    delay:0.3 *(index+1),
+                    scrollTrigger:{
+                        trigger:card,
+                        start:'top bottom-=100'
+                    }
+                }
+            )
+        })
         gsap.fromTo(
             sectionRef.current,
             {opacity:0},
