f3216014fc8c7e0e95c279dfd1b31a71efb3b87c
Yaddalapalli-Charan-Kumar-Naidu <charankumarnaidu2004@gmail.com>
2025-04-13 10:28:57 +0530

    navbar implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import {navLinks} from '../constants'
 const Navbar = () => {
+  const [scrolled,setScrolled]=useState(false);
+  useEffect(()=>{
+    const handleScroll=()=>{
+      const isScrolled=window.scrollY>10;
+      setScrolled(true);
+    }
+    window.addEventListener('scroll',handleScroll);
+    return ()=> window.removeEventListener('scroll',handleScroll)
+  },[])
   return (
-    <div>Navbar</div>
+  <header className={`navbar ${scrolled ? 'scrolled':'not-scrolled'}`}>
+      <div className="inner">
+        <a href="#hero" className='logo'>
+          Charan
+        </a>
+
+        <nav className='desktop'>
+          <ul>
+            {navLinks.map(({link,name})=>(
+              <li key={name} className='group'>
+                <a href={link}>
+                  <span>{name}</span>
+                  <span className='underline'/>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <a href="#contact" className='contact-btn'>
+          <div className="inner">
+            <span>Contact ME</span>
+          </div>
+        </a>
+      </div>
+    </header>
   )
 }
 
