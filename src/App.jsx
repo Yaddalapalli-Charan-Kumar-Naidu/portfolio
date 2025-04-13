858faa56e5295c52905e031f29b6fbe9e504ba65
Yaddalapalli-Charan-Kumar-Naidu <charankumarnaidu2004@gmail.com>
2025-04-13 10:46:26 +0530

    logo showcase implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './components/Hero/Hero'
 import ShowcaseSection from './components/ShowcaseSection'
 import Navbar from './components/Navbar'
+import LogoShowcase from './components/LogoShowcase'
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <ShowcaseSection/>
+      <LogoShowcase/>
     </>
   )
 }
